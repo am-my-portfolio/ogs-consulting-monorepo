b64f7f56af38c636e2ee7af8175dcfee0728a143
Afshan Martin <amartin@purepm.co>
2024-10-22 11:50:17 -0600

    logos

--- src/components/layout/SideMenu.vue
+++ src/components/layout/SideMenu.vue
@@ -2,12 +2,12 @@
   <div
     class="flex grow flex-col gap-y-5 overflow-y-auto border-r border-pop-secondary bg-primary px-2"
   >
-    <div class="flex mt-2 shrink-0 items-center">
-      <img src="/favicon.png" alt="logo" class="w-20" />
+    <div class="flex mt-2 shrink-0 justify-center">
+      <img src="/logo.png" alt="logo" class="w-52" />
 
-      <span class="border-b border-pop-secondary text-pop-secondary ml-4">
+      <!-- <span class="border-b border-pop-secondary text-pop-secondary ml-4">
         We Are SAM
-      </span>
+      </span> -->
     </div>
 
     <SideNavigation :level="1" :items="navigation" />
